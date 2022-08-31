2eb396fdd325c2c04e4c459ad56b00f41eaa4154
Ferdinando Villa <ferdinando.villa@gmail.com>
2022-08-31 13:19:01 +0200

    Cosmetics, alignment

--- src/test/java/org/integratedmodelling/klab/api/test/HeCoTests.java
+++ src/test/java/org/integratedmodelling/klab/api/test/HeCoTests.java
@@ -16,65 +16,66 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * To run the test suite on a remote engine, a ~/.klab/testcredential.properties file must be
- * present and contain username and password for the test engine, whose URL defaults to the
- * development engine at BC3 and can be changed using the 'engine' property.
+ * To run the test suite on a remote engine, a ~/.klab/testcredential.properties
+ * file must be present and contain username and password for the test engine,
+ * whose URL defaults to the development engine at BC3 and can be changed using
+ * the 'engine' property.
  * 
  * @author Ferd
  *
  */
 public class HeCoTests {
 
-    static String[] indicators = {"im:Indicator value of ecology:Biodiversity",
-            "im:Indicator value of ecology:Ecosystem for es:ClimateRegulation",
-            "im:Indicator es.nca:Condition of demography:SocialStructure",
-            "im:Indicator es.nca:Condition of earth:Aquatic ecology:Ecosystem"};
+	static String[] indicators = { "im:Indicator value of ecology:Biodiversity",
+			"im:Indicator value of ecology:Ecosystem for es:ClimateRegulation",
+			"im:Indicator es.nca:Condition of demography:SocialStructure",
+			"im:Indicator es.nca:Condition of earth:Aquatic ecology:Ecosystem" };
 
-    protected Klab klab;
-    protected String username;
-    protected String password;
-    protected String testEngine;
+	protected Klab klab;
+	protected String username;
+	protected String password;
+	protected String testEngine;
 
-    @Before
-    public void connect() {
-        this.klab = createClient();
-        assert klab.isOnline();
-    }
+	@Before
+	public void connect() {
+		this.klab = createClient();
+		assert klab.isOnline();
+	}
 
-    protected Klab createClient() {
-        readCredentials();
-        return Klab.create(testEngine, username, password);
-    }
+	protected Klab createClient() {
+		readCredentials();
+		return Klab.create(testEngine, username, password);
+	}
 
-    @After
-    public void disconnect() throws IOException {
-        if (klab.isOnline()) {
-            klab.close();
-        }
-    }
+	@After
+	public void disconnect() throws IOException {
+		if (klab.isOnline()) {
+			klab.close();
+		}
+	}
 
-    public void readCredentials() {
-        Properties properties = new Properties();
-        try (InputStream input = new FileInputStream(new File(
-                System.getProperty("user.home") + File.separator + ".klab" + File.separator + "testcredentials.properties"))) {
-            properties.load(input);
-        } catch (IOException e) {
-            throw new KlabResourceNotFoundException(
-                    "can't open ~/.klab/testcredentials.properties with username and passwords for test engine");
-        }
-        this.username = properties.getProperty("username", "username");
-        this.password = properties.getProperty("password", "password");
-        this.testEngine = properties.getProperty("engine", "https://developers.integratedmodelling.org/modeler");
-    }
+	public void readCredentials() {
+		Properties properties = new Properties();
+		try (InputStream input = new FileInputStream(new File(System.getProperty("user.home") + File.separator + ".klab"
+				+ File.separator + "testcredentials.properties"))) {
+			properties.load(input);
+		} catch (IOException e) {
+			throw new KlabResourceNotFoundException(
+					"can't open ~/.klab/testcredentials.properties with username and passwords for test engine");
+		}
+		this.username = properties.getProperty("username", "username");
+		this.password = properties.getProperty("password", "password");
+		this.testEngine = properties.getProperty("engine", "https://developers.integratedmodelling.org/modeler");
+	}
 
-    @Test
-    public void biodiversityIndicator() throws Exception {
-        Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
-        assert colombia != null;
-        Observation biodiversityIndicator = colombia.submit(Observable.create(indicators[0])).get();
-        assert biodiversityIndicator != null && !biodiversityIndicator.isEmpty();
-        System.out.println("DATA RANGE: " + biodiversityIndicator.getDataRange());
-        System.out.println("AGGREGATED: " + biodiversityIndicator.getAggregatedValue());
-        System.out.println("SCALAR VAL: " + biodiversityIndicator.getScalarValue());
-    }
+	@Test
+	public void biodiversityIndicator() throws Exception {
+		Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
+		assert colombia != null;
+		Observation biodiversityIndicator = colombia.submit(Observable.create(indicators[0])).get();
+		assert biodiversityIndicator != null && !biodiversityIndicator.isEmpty();
+		assert biodiversityIndicator.getAggregatedValue() instanceof Number
+				&& ((Number) biodiversityIndicator.getAggregatedValue()).doubleValue() > 0;
+		assert biodiversityIndicator.getScalarValue() == null;
+	}
 }
