6a2a1c80643c9482d40aa48ebdb02c41c73fe945
Ferdinando Villa <ferdinando.villa@gmail.com>
2022-11-22 12:14:15 +0100

    Smaller context for HeCo tests

--- src/test/java/org/integratedmodelling/klab/api/test/HeCoTests.java
+++ src/test/java/org/integratedmodelling/klab/api/test/HeCoTests.java
@@ -10,16 +10,16 @@ import org.integratedmodelling.klab.api.Context;
 import org.integratedmodelling.klab.api.Klab;
 import org.integratedmodelling.klab.api.Observable;
 import org.integratedmodelling.klab.api.Observation;
+import org.integratedmodelling.klab.common.Geometry;
 import org.integratedmodelling.klab.exceptions.KlabResourceNotFoundException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * To run the test suite on a remote engine, a ~/.klab/testcredential.properties
- * file must be present and contain username and password for the test engine,
- * whose URL defaults to the development engine at BC3 and can be changed using
- * the 'engine' property.
+ * To run the test suite on a remote engine, a ~/.klab/testcredential.properties file must be
+ * present and contain username and password for the test engine, whose URL defaults to the
+ * development engine at BC3 and can be changed using the 'engine' property.
  * 
  * @author Ferd
  *
@@ -31,59 +31,65 @@ public class HeCoTests {
             "im:Indicator (es.nca:Condition of earth:Aquatic ecology:Ecosystem)",
             "im:Indicator (es.nca:Condition of demography:Human demography:Community)"};
 
-	protected Klab klab;
-	protected String username;
-	protected String password;
-	protected String testEngine;
+    static Geometry centralColombia = Geometry.create(
+            "τ0(1){ttype=LOGICAL,period=[1609459200000 1640995200000],tscope=1.0,tunit=YEAR}S2(934,631){bbox=[-75.2281407807369 -72.67107290964314 3.5641500380320963 5.302943221927137],shape=00000000030000000100000005C0522AF2DBCA0987400C8361185B1480C052CE99DBCA0987400C8361185B1480C052CE99DBCA098740153636BF7AE340C0522AF2DBCA098740153636BF7AE340C0522AF2DBCA0987400C8361185B1480,proj=EPSG:4326}");
 
-	@Before
-	public void connect() {
-		this.klab = createClient();
-		assert klab.isOnline();
-	}
+    protected Klab klab;
+    protected String username;
+    protected String password;
+    protected String testEngine;
 
-	protected Klab createClient() {
-		readCredentials();
-		return Klab.create(testEngine, username, password);
-	}
+    @Before
+    public void connect() {
+        this.klab = createClient();
+        assert klab.isOnline();
+    }
 
-	@After
-	public void disconnect() throws IOException {
-		if (klab.isOnline()) {
-			klab.close();
-		}
-	}
+    protected Klab createClient() {
+        readCredentials();
+        return Klab.create(testEngine, username, password);
+    }
 
-	public void readCredentials() {
-		Properties properties = new Properties();
-		try (InputStream input = new FileInputStream(new File(System.getProperty("user.home") + File.separator + ".klab"
-				+ File.separator + "testcredentials.properties"))) {
-			properties.load(input);
-		} catch (IOException e) {
-			throw new KlabResourceNotFoundException(
-					"can't open ~/.klab/testcredentials.properties with username and passwords for test engine");
-		}
-		this.username = properties.getProperty("username", "username");
-		this.password = properties.getProperty("password", "password");
-		this.testEngine = properties.getProperty("engine", "https://developers.integratedmodelling.org/modeler");
-	}
+    @After
+    public void disconnect() throws IOException {
+        if (klab.isOnline()) {
+            klab.close();
+        }
+    }
 
-	@Test
-	public void biodiversityIndicator() throws Exception {
-		Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
-		assert colombia != null;
+    public void readCredentials() {
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream(new File(
+                System.getProperty("user.home") + File.separator + ".klab" + File.separator + "testcredentials.properties"))) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new KlabResourceNotFoundException(
+                    "can't open ~/.klab/testcredentials.properties with username and passwords for test engine");
+        }
+        this.username = properties.getProperty("username", "username");
+        this.password = properties.getProperty("password", "password");
+        this.testEngine = properties.getProperty("engine", "https://developers.integratedmodelling.org/modeler");
+    }
 
-		for (String indicator : indicators) {
-			Observation biodiversityIndicator = colombia.submit(Observable.create(indicator)).get();
+    @Test
+    public void biodiversityIndicator() throws Exception {
 
-			assert biodiversityIndicator != null && !biodiversityIndicator.isEmpty();
+        Context colombia = klab.submit(Observable.create("earth:Region"), centralColombia).get();
 
-			System.out.println(indicator + " = " + biodiversityIndicator.getAggregatedValue());
+//        Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
+        assert colombia != null;
 
-			assert biodiversityIndicator.getAggregatedValue() instanceof Number
-					&& ((Number) biodiversityIndicator.getAggregatedValue()).doubleValue() > 0;
-			assert biodiversityIndicator.getScalarValue() == null;
-		}
+        for (String indicator : indicators) {
+            Observation biodiversityIndicator = colombia.submit(Observable.create(indicator)).get();
 
-	}
+            assert biodiversityIndicator != null && !biodiversityIndicator.isEmpty();
+
+            System.out.println(indicator + " = " + biodiversityIndicator.getAggregatedValue());
+
+            assert biodiversityIndicator.getAggregatedValue() instanceof Number
+                    && ((Number) biodiversityIndicator.getAggregatedValue()).doubleValue() > 0;
+            assert biodiversityIndicator.getScalarValue() == null;
+        }
+
+    }
 }
--- src/test/java/org/integratedmodelling/klab/api/test/HeCoTestsLocal.java
+++ src/test/java/org/integratedmodelling/klab/api/test/HeCoTestsLocal.java
@@ -6,16 +6,16 @@ import org.integratedmodelling.klab.api.Context;
 import org.integratedmodelling.klab.api.Klab;
 import org.integratedmodelling.klab.api.Observable;
 import org.integratedmodelling.klab.api.Observation;
+import org.integratedmodelling.klab.common.Geometry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * HeCo test suite configured for a local engine. In order for the test to run,
- * the k.LAB engine and modeler must be running on the local machine, and the
- * project https://bitbucket.org/integratedmodelling/aries.heco.git must have
- * been imported in the modeler and set to the develop branch prior to starting
- * the engine.
+ * HeCo test suite configured for a local engine. In order for the test to run, the k.LAB engine and
+ * modeler must be running on the local machine, and the project
+ * https://bitbucket.org/integratedmodelling/aries.heco.git must have been imported in the modeler
+ * and set to the develop branch prior to starting the engine.
  * 
  * @author Ferd
  *
@@ -27,72 +27,76 @@ public class HeCoTestsLocal {
             "im:Indicator (es.nca:Condition of earth:Aquatic ecology:Ecosystem)",
             "im:Indicator (es.nca:Condition of demography:Human demography:Community)"};
 
-	protected Klab klab;
+    static Geometry centralColombia = Geometry.create(
+            "τ0(1){ttype=LOGICAL,period=[1609459200000 1640995200000],tscope=1.0,tunit=YEAR}S2(934,631){bbox=[-75.2281407807369 -72.67107290964314 3.5641500380320963 5.302943221927137],shape=00000000030000000100000005C0522AF2DBCA0987400C8361185B1480C052CE99DBCA0987400C8361185B1480C052CE99DBCA098740153636BF7AE340C0522AF2DBCA098740153636BF7AE340C0522AF2DBCA0987400C8361185B1480,proj=EPSG:4326}");
 
-	@Before
-	public void connect() {
-		this.klab = createClient();
-		assert klab.isOnline();
-	}
+    protected Klab klab;
 
-	protected Klab createClient() {
-		return Klab.create();
-	}
+    @Before
+    public void connect() {
+        this.klab = createClient();
+        assert klab.isOnline();
+    }
 
-	@After
-	public void disconnect() throws IOException {
-		if (klab.isOnline()) {
-			klab.close();
-		}
-	}
+    protected Klab createClient() {
+        return Klab.create();
+    }
 
-	private void computeIndicator(String indicatorObservable) throws Exception {
-		Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
-		assert colombia != null;
-		Observation indicator = colombia.submit(Observable.create(indicatorObservable)).get();
-		assert indicator != null && !indicator.isEmpty();
-		System.out.println(indicator + " = " + indicator.getAggregatedValue());
-		assert indicator.getAggregatedValue() instanceof Number
-				&& ((Number) indicator.getAggregatedValue()).doubleValue() > 0;
-		assert indicator.getScalarValue() == null;
-	}
-	
-	@Test
-	public void firstIndicatorSeparately() throws Exception {
-		computeIndicator(indicators[0]);
-	}
+    @After
+    public void disconnect() throws IOException {
+        if (klab.isOnline()) {
+            klab.close();
+        }
+    }
 
-	@Test
-	public void secondIndicatorSeparately() throws Exception {
-		computeIndicator(indicators[1]);
-	}
-	
-	@Test
-	public void thirdIndicatorSeparately() throws Exception {
-		computeIndicator(indicators[2]);
-	}
-	
-	@Test
-	public void fourthIndicatorSeparately() throws Exception {
-		computeIndicator(indicators[3]);
-	}
+    private void computeIndicator(String indicatorObservable) throws Exception {
+        // Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
+        Context colombia = klab.submit(Observable.create("earth:Region"), centralColombia).get();
+        assert colombia != null;
+        Observation indicator = colombia.submit(Observable.create(indicatorObservable)).get();
+        assert indicator != null && !indicator.isEmpty();
+        System.out.println(indicator + " = " + indicator.getAggregatedValue());
+        assert indicator.getAggregatedValue() instanceof Number && ((Number) indicator.getAggregatedValue()).doubleValue() > 0;
+        assert indicator.getScalarValue() == null;
+    }
 
-	@Test
-	public void allIndicatorsSequentially() throws Exception {
-		Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
-		assert colombia != null;
+    @Test
+    public void firstIndicatorSeparately() throws Exception {
+        computeIndicator(indicators[0]);
+    }
 
-		int success = 0;
-		for (String indicator : indicators) {
-			Observation observedIndicator = colombia.submit(Observable.create(indicator)).get();
-			if (observedIndicator.isEmpty()) {
-				System.out.println("Observation of " + indicator + " failed");
-				continue;
-			}
-			success ++;
-			System.out.println(indicator + " = " + observedIndicator.getAggregatedValue());
-		}
-		
-		assert success == indicators.length;
-	}
+    @Test
+    public void secondIndicatorSeparately() throws Exception {
+        computeIndicator(indicators[1]);
+    }
+
+    @Test
+    public void thirdIndicatorSeparately() throws Exception {
+        computeIndicator(indicators[2]);
+    }
+
+    @Test
+    public void fourthIndicatorSeparately() throws Exception {
+        computeIndicator(indicators[3]);
+    }
+
+    @Test
+    public void allIndicatorsSequentially() throws Exception {
+//        Context colombia = klab.submit("aries.heco.locations.colombia_continental").get();
+        Context colombia = klab.submit(Observable.create("earth:Region"), centralColombia).get();
+        assert colombia != null;
+
+        int success = 0;
+        for (String indicator : indicators) {
+            Observation observedIndicator = colombia.submit(Observable.create(indicator)).get();
+            if (observedIndicator.isEmpty()) {
+                System.out.println("Observation of " + indicator + " failed");
+                continue;
+            }
+            success++;
+            System.out.println(indicator + " = " + observedIndicator.getAggregatedValue());
+        }
+
+        assert success == indicators.length;
+    }
 }
